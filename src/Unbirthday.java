995644208b72756ad4f38873eb9399113444e890
harrisoncliew <league@WTS14-4.attlocal.net>
2018-08-06 11:58:01 -0700

    First part of Level 0 module 1

---
+++ src/Unbirthday.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+
+public class Unbirthday {
+	public static void main(String[] args) {
+		String birthday = JOptionPane.showInputDialog("When is your birthday? (m/d");
+		if (cool.equals(""))
+
+	}
+
+}
